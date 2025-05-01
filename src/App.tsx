3b484d78138b7588d5d860906c86b812a2d434ee
alerb <alejandroroblesb@hotmail.com>
2025-05-01 02:01:03 -0600

    Fix build errors for Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+
 import Home from './../src/assets/components/pages/Home'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <main className="min-h-screen">
